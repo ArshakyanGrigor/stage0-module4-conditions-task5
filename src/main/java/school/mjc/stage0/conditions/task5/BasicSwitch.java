93ef6938917fac913ef7568f13a18aa5b5239803
ArshakyanGrigor <grigor.arshakyan1994@mail.ru>
2022-12-18 20:50:32 +0400

    Updated

--- src/main/java/school/mjc/stage0/conditions/task5/BasicSwitch.java
+++ src/main/java/school/mjc/stage0/conditions/task5/BasicSwitch.java
@@ -1,17 +1,20 @@
 package school.mjc.stage0.conditions.task5;
 
 public class BasicSwitch {
-    public void main(String[] args) {
+    public static void main(String[] args) {
         int a = 20;
         switch (a) {
+            default:
+                System.out.println("default");
+                break;
             case 7:
                 System.out.println("seven");
                 break;
             case 10:
                 System.out.println("ten");
                 break;
-            default:
-                System.out.println("default");
+            case 20:
+                System.out.println("twenty");
                 break;
         }
     }
